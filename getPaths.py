44c7dee80f9ffd4e34ee876dece4640f29aa0680
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-01-30 13:48:02 +0100

    .pkl and .csv per each cancer type

--- getPaths.py
+++ getPaths.py
@@ -33,10 +33,12 @@ import topo_fun as tp
 path = "/Users/rosherbal/Projects/network_tools"
 os.chdir(path)
 
-net_type =  ["omnipath", "GWpkn"]
+net_type =  ['omnipathNOsign']#["omnipath", "GWpkn"]
+save_folder = 'CMP_noSign_subpaths/' #CMP_subpaths
 
 carnival_folder = "../00-Networks/footprints_tissue_specific/CARNIVAL"
 smpl_res = os.listdir(carnival_folder + "/" + net_type[0])
+#smpl_res.remove('SIDM00319')
 
 meta_samples = pd.read_csv('../00-CMP/metadata/model_list_2019-04-04_0916.csv',
                             sep = ",")
@@ -44,53 +46,44 @@ meta_samples = meta_samples[meta_samples['model_id'].isin(smpl_res)]
 
 cancer_type = list(set(meta_samples['cancer_type']))
 
-cancer_tissue = {}
-lstats = []
-
 #=======
 # Main #
 #=======
 
 # Create the structure
-# {c_type:{network:sample:[icar, nodI, nodE, isoNode, subpaths]}}
+# {sample:[icar, nodI, nodE, isoNode, subpaths]}
 # also a pd.df with the metrics of all the previous objects
 for i in tqdm(range(len(cancer_type))):
     ctype = cancer_type[i]
-    samples = meta_samples[meta_samples['cancer_type'] == ctype]["model_id"].tolist()
-    netWord = {}
     print(ctype)
+    samples = meta_samples[meta_samples['cancer_type'] == ctype]["model_id"].tolist()
     for network in net_type:
-        netWord[network] = {}
-        print(network)
+        lstats = []
+        netWord = {}
         for sample in samples:
+
             # read information per sample
             pcar = carnival_folder + "/" + network + "/" + sample
-            smpl_pd = pd.read_csv(pcar + "/weightedModel_1.txt", sep = "\t")
-            smpl_nAtt = pd.read_csv(pcar + "/nodesAttributes_1.txt", sep = "\t")
-
-            # create igraph object with weights and node attributes
-            nodI, nodE, icar = tp.CARNIVAL2igraph_convert(smpl_pd)
-            icar, isoNode = tp.CARNIVALnode_attribute(icar, smpl_nAtt)
-
-            # finde all subpaths from initial nodes to effectors
-            paths = tp.pathfinder(icar, nodI, nodE)
 
             # add information to a dictionary
-            netWord[network][sample] = [icar, nodI, nodE, isoNode, paths]
+            netWord[sample] = tp.getTopology(Cfile = pcar)
 
             # get some metrics
             aux = {"sample": sample, "cancer_tissue": ctype,
-                     "network": network, "vertices": icar.vcount(),
-                     "edges": icar.ecount(), "iniciators": len(nodI),
-                     "effectors": len(nodE), "isolated": len(isoNode),
-                     "subpaths": len(paths)}
+                     "network": network,
+                     "vertices": netWord[sample][0].vcount(),
+                     "edges": netWord[sample][0].ecount(),
+                     "iniciators": len(netWord[sample][1]),
+                     "effectors": len(netWord[sample][2]),
+                     "isolated": len(netWord[sample][3]),
+                     "subpaths": len(netWord[sample][4])}
             lstats.append(aux)
 
-    cancer_tissue[ctype] = netWord
-    with open('CMP_subpaths/' + ctype + '_netextend-omnipath_GWpkn.pkl', 'wb') \
-                as ofile:
-        pickle.dump(cancer_tissue[ctype], ofile)
-    ofile.close()
+        with open(save_folder + ctype + '_netextend-' + net_type[0] + '.pkl', 'wb') \
+                    as ofile:
+            pickle.dump(netWord, ofile)
+        ofile.close()
 
-stats = pd.DataFrame(lstats)
-stats.to_csv('CMP_subpaths/netsum-omnipath_GWpkn.csv', header=True, index=None)
+        stats = pd.DataFrame(lstats)
+        stats.to_csv(save_folder + ctype + '_netsum-' + net_type[0] + '.csv',
+                        header=True, index=None)
