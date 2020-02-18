2f1a98cf6d14f73543f2e47abd14e82eceafc311
rosherbal <rosa.hernansaiz@bioquant.uni-heidelberg.de>
2020-02-18 15:26:05 +0100

    3 new functions to ride out of igraph and use networkX

--- topo_fun.py
+++ topo_fun.py
@@ -7,6 +7,110 @@ DESCRIPTION : topological functions to deal with CARNIVAL's results.
 LICENSE : GPL-v3
 """
 
+def carnival2directGraph(weigthSample, inverse=True):
+    """
+    Creates a dirct graph from CARNIVAL's weight's file.
+    :param weithSample string or pd.df: weightedModel_1.txt from CARNIVAL
+    :param inverse boolean: type of carnival analysis. inverse=True (default)
+    :return: DG (direct graph)
+    :rtype: networkX graph
+    """
+    import pandas as pd
+    import networkx as nx
+
+    if type(weigthSample) == str:
+        wm = pd.read_csv(weigthSample, sep='\t') # read file
+    else:
+        wm = weigthSample
+
+    if inverse:
+        wm = wm[wm.Node1 != 'Perturbation'] # remove perturbation node
+        wm.reset_index(inplace = True, drop=True) # reset indexes
+
+    dupEdge = wm[wm[['Node1', 'Node2']].duplicated(keep=False)] # get duplicates
+
+    if dupEdge.shape[0] > 0:
+
+        idxDE = dupEdge[['Node1', 'Node2']].groupby(dupEdge[['Node1', 'Node2']].columns.tolist()).apply(lambda x: tuple(x.index)).tolist()
+
+        print('Number of repeated edges:', dupEdge.shape[0]/2)
+
+        # get the highest value for the repeated edge (keeping sign)
+        index2drop = []
+        for dupIndex in idxDE:
+            weight = [wm.iloc[dupIndex[0]]['Weight'], wm.iloc[dupIndex[1]]['Weight']]
+            i = weight.index(min(weight))
+            index2drop.append(dupIndex[i])
+
+        wm.drop(labels=[dupIndex[i]], axis='index', inplace=True)
+
+    # create direct graph
+    DG = nx.DiGraph()
+
+    auxL = []
+    for row in wm.values:
+    	auxL.append((row[0], row[2], int(row[1])*int(row[3])))
+
+    DG.add_weighted_edges_from(auxL)
+
+    return(DG)
+
+def get_initiators_effectors(weigthSample, inverse=True):
+    """
+    Get initiators (initial nodes) and effectors (ending nodes)
+    from CARNIVAL weightedModel_1
+    :param weithSample string or pd.df: weightedModel_1.txt from CARNIVAL
+    :param inverse boolean: type of carnival analysis. inverse=True (default)
+    :return: iNodes (initial nodes), eNodes (effectors)
+    :rtype: set
+    """
+
+    import pandas as pd
+
+    if type(weigthSample) == str:
+        wm = pd.read_csv(weigthSample, sep='\t') # read file
+    else:
+        wm = weigthSample
+
+    if inverse:
+        wm = wm[wm.Node1 != 'Perturbation'] # remove perturbation node
+        wm.reset_index(inplace = True) # reset indexes
+
+    iNodes = set(wm['Node1']) - set(wm['Node2'])
+    eNodes = set(wm['Node2']) - set(wm['Node1'])
+
+    return(iNodes, eNodes)
+#
+def get_measurments(DG):
+    """
+    Get network and node-based measurments for a network as networkX.DiGraph
+    :param DG networkx.DiGraph: directed graph frim networkX
+    :return: measure with values: number nodes, edges, network's density,
+                                  avg betweenness centrality,
+                                  avg degree centrality
+                                  and avg eigenvector centrality
+    :rtype: dict
+    """
+    import networkx as nx
+    import numpy as np
+
+    measure = {}
+    measure['nNodes'] = nx.number_of_nodes(DG)
+    measure['nEdges'] = nx.number_of_edges(DG)
+    measure['density'] = nx.density(DG)
+    #measure['diameter'] = nx.diameter(DG)
+    bc = nx.betweenness_centrality(DG)
+    measure['avg betweenness centrality'] = np.mean(list(bc.values()))
+    dc = nx.degree_centrality(DG)
+    measure['avg degree centrality'] = np.mean(list(dc.values()))
+    #cc = nx.closeness_centrality(DG)
+    #measure['avg closeness centrality'] = np.mean(list(cc.values()))
+    #ec = nx.eccentricity(DG)
+    #measure['avg eccentricity'] = np.mean(list(ec.values()))
+    ev = nx.eigenvector_centrality(DG)
+    measure['avg eigenvector centrality'] = np.mean(list(ev.values()))
+    return(measure)
+
 def CARNIVAL2igraph_convert(weigthSample):
     """
     Transform CARNIVAL's output read as pd.df into igraph.
